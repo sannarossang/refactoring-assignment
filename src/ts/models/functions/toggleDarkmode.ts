1ab18da5dac32ad13675abd580645d1961be97e8
sannarossang <sanna.rossang@gmail.com>
2023-03-15 15:11:55 +0100

    (fix): updated accessibility for images

--- src/ts/models/functions/toggleDarkmode.ts
+++ src/ts/models/functions/toggleDarkmode.ts
@@ -4,16 +4,16 @@ const toggleLightModeButton = document.querySelector(
   ".toggle-btn"
 ) as HTMLButtonElement;
 toggleLightModeButton.addEventListener("click", toggleLightMode);
-logMessage(toggleLightModeButton);
+console.log(toggleLightModeButton);
 
 export function toggleLightMode() {
   document.body.classList.toggle("darkmode");
   if (document.body.classList.contains("darkmode")) {
     toggleLightModeButton.innerHTML = "Välj mörkt läge";
-    logMessage("mörkt läge");
+    console.log("mörkt läge");
   } else {
     toggleLightModeButton.innerHTML = "Välj ljust läge";
-    logMessage("ljust läge");
+    console.log("ljust läge");
   }
 }
 
