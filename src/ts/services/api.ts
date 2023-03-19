92d84f14012b0aee377ed2eee8631ee36d36a3f8
sannarossang <sanna.rossang@gmail.com>
2023-03-19 19:26:11 +0100

    (build): added url in .env

--- src/ts/services/api.ts
+++ src/ts/services/api.ts
@@ -2,9 +2,19 @@ import axios from "axios";
 import type { IPodcast } from "../models/IPodcast";
 import type { PodcastResponse } from "../models/PodcastResponse";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+const srUrl: string = import.meta.env.VITE_APP_ENDPOINT;
+
 export async function getPodcasts(): Promise<IPodcast[]> {
-  const response = await axios.get<PodcastResponse>(
-    "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false"
-  );
-  return response.data.programs;
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const response = await axios.get<PodcastResponse>(srUrl);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return response.data.programs;
+  } catch (error) {
+    console.log("Error when getting data from api...", error);
+    return [];
+  }
 }
