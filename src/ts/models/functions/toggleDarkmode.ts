ef472590144eeadf149b98d35bebc9d193d514f4
sannarossang <sanna.rossang@gmail.com>
2023-03-19 19:28:16 +0100

    (feature): fixed logging-function

--- src/ts/models/functions/toggleDarkmode.ts
+++ src/ts/models/functions/toggleDarkmode.ts
@@ -3,16 +3,15 @@ import { logMessage } from "./logging";
 // eslint-disable-next-line
 const toggleLightModeButton = document.querySelector(".toggle-btn") as HTMLButtonElement;
 toggleLightModeButton.addEventListener("click", toggleLightMode);
-console.log(toggleLightModeButton);
 
 export function toggleLightMode(): void {
   document.body.classList.toggle("darkmode");
   if (document.body.classList.contains("darkmode")) {
     toggleLightModeButton.innerHTML = "Välj mörkt läge";
-    console.log("mörkt läge");
+    logMessage("mörkt läge");
   } else {
     toggleLightModeButton.innerHTML = "Välj ljust läge";
-    console.log("ljust läge");
+    logMessage("ljust läge");
   }
 }
 
